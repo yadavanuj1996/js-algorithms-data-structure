80f53f2c937db423b04c91d85516a0130b1b7a20
yadavanuj1996 <yadavanuj@hotmail.com>
2019-10-21 01:13:13 +0530

    Add Queue DS.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {executeLinkedLinkTest} from './linked-list/test/jasmine.test.js';
 import {executeDoublyLinkedLinkTest} from './doubly-linked-list/test/test.js';
+import {executeQueueTest} from './Queue/test/test.js';
 
 //executeLinkedLinkTest();
-executeDoublyLinkedLinkTest();
+//executeDoublyLinkedLinkTest();
+executeQueueTest();
