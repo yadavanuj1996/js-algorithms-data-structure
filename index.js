a4a0b88f7bb26839ed80948fa0b8d4efc9030d89
yadavanuj1996 <yadavanuj@hotmail.com>
2019-10-21 01:59:18 +0530

    Add Stack DS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import {executeLinkedLinkTest} from './linked-list/test/jasmine.test.js';
 import {executeDoublyLinkedLinkTest} from './doubly-linked-list/test/test.js';
 import {executeQueueTest} from './Queue/test/test.js';
-
+import {executeStackTest} from './Stack/test/test.js';
 //executeLinkedLinkTest();
 //executeDoublyLinkedLinkTest();
-executeQueueTest();
+//executeQueueTest();
+executeStackTest();
